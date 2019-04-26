12029ddac5df2b39f81bb43365c944950d47be85
riagoncalves <ricardo.goncalves252@gmail.com>
2019-04-26 17:32:51 +0100

    fully gulp setup

--- gulpfile.js/tasks/javascript.js
+++ gulpfile.js/tasks/javascript.js
@@ -8,7 +8,6 @@ var babel = require('gulp-babel');
 
 gulp.task('javascript', function () {
   return gulp.src(config.src)
-  	.pipe(babel({presets: ['es2015']}))
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest(config.dest))
