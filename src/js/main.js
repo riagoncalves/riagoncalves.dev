3d079d6bc6a4e4b59b891950fb03e6592ab3d650
unknown <ricardo.goncalves252@gmail.com>
2019-04-30 13:11:18 +0100

    added basic structure and instagram profile picture

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,19 @@
 "use strict";
 /*global document: false */
 
-function init() {
+function loadIgPicture() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.querySelector('.ig-profile').src = JSON.parse(this.responseText).data.profile_picture;
+    }
+  };
+  xhttp.open("GET", "https://api.instagram.com/v1/users/3308902477/?access_token=3308902477.70637ff.65f32e6d0998400c840a50cb55b09140", true);
+  xhttp.send();
+}
 
+function init() {
+  loadIgPicture();
 }
 
 function ready() {
