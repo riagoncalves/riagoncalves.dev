61049baecef80dfa3935c15fafcba1bbf5f73849
unknown <ricardo.goncalves252@gmail.com>
2021-04-04 12:08:15 +0100

    projects section

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+"use strict";
+/*global document: false */
+
+function init() {
+}
+
+function ready() {
+  if (document.readyState === 'complete') {
+    init();
+  } else {
+      document.addEventListener('DOMContentLoaded', init);
+  }
+
+}
+
+ready();
+
