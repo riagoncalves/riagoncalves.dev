e1cdaef822a839dd7e12a814ee82461c0a8e1b33
Ricardo Gonçalves <ricardo.goncalves252@gmail.com>
2022-07-28 23:19:01 +0100

    refactor(website): entire website in nextjs ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,39 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import Projects from '../data/projects.json'
+import Skills from '../data/skills.json'
+import Contacts from '../data/contacts.json'
+
+interface HomePageProps {
+  projects: {
+    key: string,
+    name: string,
+    website?: string,
+    github?: string,
+  }[],
+  skills: {
+    key: string,
+    name: string,
+  }[],
+  contacts: {
+    key: string,
+    link: string,
+  }[],
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      projects: Projects,
+      skills: Skills,
+      contacts: Contacts
+    }
+  }
+}
 
 
-const Home: NextPage = () => {
+const Home: NextPage<HomePageProps> = ({ projects, skills, contacts }) => {
   return (
     <div>
       <Head>
@@ -28,80 +58,83 @@ const Home: NextPage = () => {
       <main>
         <section className="relative h-screen bg-no-repeat bg-center bg-cover flex items-center justify-end bg-hero-pattern">
           <div className="max-w-screen-2xl w-full mx-auto text-right px-8 2xl:px-0">
-            <Image src="/images/profile_pic.jpg"
-                   width={150}
-                   height={150}
-                   alt="Profile Picture"
-                   className="transition-scale border border-solid border-black rounded-full" />
+            <div className='transition-scale w-36 h-36 relative ml-auto mr-0'>
+              <Image src="/images/profile_pic.jpg"
+                    layout='fill'
+                    objectFit='contain'
+                    alt="Profile Picture"
+                    className="border border-solid border-black rounded-full" />
+            </div>
             <h1>Ricardo Gonçalves</h1>
             <h2>Web Developer based in Lisbon</h2>
           </div>
           <div className="absolute w-screen h-1/5-screen bottom-0 left-0 bg-gradient-to-b from-transparent to-main"></div>
         </section>
 
-        <section className="s-2 pb-20">
+        <section className="pb-20">
           <div className="max-w-screen-2xl w-full mx-auto text-center">
             <h1>Projects</h1>
             <ul className="flex justify-center flex-wrap items-start pt-12">
-              <li className='px-5 max-w-full md:max-w-1/2 lg:max-w-1/3'>
-                <div className='relative w-80 m-4 min-h-[180px] transition-scale'>
-                  <Image layout='fill' objectFit="contain" src="/images/bot.jpg" alt="Bot" />
-                </div>
-                <p>Discord Bot</p>
-                <div className="flex justify-center items-center pt-2.5">
-                  <a href="http://shinobubot.com/" target="_blank" rel="noreferrer" className='transition-scale'>
-                    <object data="/images/website.svg" type="image/svg+xml" className='w-full px-2.5 pointer-events-none max-w-[40px]'>
-                      Website
-                    </object>
-                  </a>
-                  <a href="https://github.com/riagoncalves/shinobu" target="_blank" rel="noreferrer" className='transition-scale'>
-                    <object data="/images/github.svg" type="image/svg+xml" className='w-full px-2.5 pointer-events-none max-w-[40px]'>
-                      Github
-                    </object>
-                  </a>
-                </div>
-              </li>
-              <li className='px-5 max-w-full md:max-w-1/2 lg:max-w-1/3'>
-                <div className='relative w-80 m-4 min-h-[180px] transition-scale'>
-                  <Image layout='fill' objectFit="contain" src="/images/sarcastify.jpg" alt="Sarcastify" />
-                </div>
-                <p>Sarcastify Website</p>
-                <div className="flex justify-center items-center pt-2.5">
-                  <a href="https://sarcastify.riagoncalves.dev/" target="_blank" rel="noreferrer" className='transition-scale'>
-                    <object data="/images/website.svg" type="image/svg+xml" className='w-full px-2.5 pointer-events-none max-w-[40px]'>
-                      Website
-                    </object>
-                  </a>
-                  <a href="https://github.com/riagoncalves/sarcastify" target="_blank" rel="noreferrer" className='transition-scale'>
-                    <object data="/images/github.svg" type="image/svg+xml" className='w-full px-2.5 pointer-events-none max-w-[40px]'>
-                      Github
-                    </object>
-                  </a>
-                </div>
-              </li>
-              <li className='px-5 max-w-full md:max-w-1/2 lg:max-w-1/3'>
-                <div className='relative w-80 m-4 min-h-[180px] transition-scale'>
-                  <Image  layout='fill' objectFit="contain" src="/images/transfer.jpg" alt="Transfer" />
-                </div>
-                <p>Website Transfer Theme</p>
-                <div className="flex justify-center items-center pt-2.5">
-                  <a href="https://transfer-template.netlify.app/" target="_blank" rel="noreferrer" className='transition-scale'>
-                    <object data="/images/website.svg" type="image/svg+xml" className='w-full px-2.5 pointer-events-none max-w-[40px]'>
-                      Website
-                    </object>
-                  </a>
-                  <a href="https://github.com/riagoncalves/transfer-website-theme" target="_blank" rel="noreferrer" className='transition-scale'>
-                    <object data="/images/github.svg" type="image/svg+xml" className='w-full px-2.5 pointer-events-none max-w-[40px]'>
-                      Github
-                    </object>
-                  </a>
-                </div>
-              </li>
+              {projects.map((project, index) => (
+                <li key={index} className='px-5 max-w-full md:max-w-1/2 lg:max-w-1/3'>
+                  <div className='relative w-80 m-4 min-h-[180px] transition-scale'>
+                    <Image layout='fill' objectFit="contain" src={`/images/${project.key}.jpg`} alt={project.key} />
+                  </div>
+                  <p>{project.name}</p>
+                  <div className="flex justify-center items-center pt-2.5">
+                    {project.website && (
+                      <a href={project.website} target="_blank" rel="noreferrer" className='transition-scale'>
+                        <object data="/images/website.svg" type="image/svg+xml" className='w-full px-2.5 pointer-events-none max-w-[40px]'>
+                          Website
+                        </object>
+                      </a>
+                    )}
+                    {project.github && (
+                      <a href={project.github} target="_blank" rel="noreferrer" className='transition-scale'>
+                      <object data="/images/github.svg" type="image/svg+xml" className='w-full px-2.5 pointer-events-none max-w-[40px]'>
+                        Github
+                      </object>
+                    </a>
+                    )}  
+                  </div>
+                </li>
+              ))}
             </ul>
-
           </div>
-      </section>
+        </section>
+
+        <section className="pb-0">
+          <div className="max-w-screen-2xl w-full mx-auto text-center after:mt-32 after:h-[3px] after:block after:bg-divider">
+            <h1>Skills</h1>
+            <ul className="max-w-3xl pt-12 m-auto flex justify-center items-center flex-wrap">
+              {skills.map((skill, index) => (
+                <li key={index}>
+                  <object data={`/images/${skill.key}.svg`} type="image/svg+xml" className='w-20 p-4 transition-scale'>
+                    { skill.name }
+                  </object>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
       </main>
+
+      <footer className="p-12">
+        <div className="flex max-w-screen-2xl w-full mx-auto">
+          <p>Contacts</p>
+          <ul className="flex flex-1 justify-end">
+            {contacts.map((contact, index) => (
+              <li key={index} className="ml-5 w-8 transition-scale">
+                <a href={contact.link} target="_blank" rel="noreferrer" className="flex">
+                  <div className='relative w-8 h-8'>
+                    <Image  layout='fill' objectFit="contain" src={`/images/${contact.key}.svg`} alt={contact.key} />
+                  </div>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </footer>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
       },
       backgroundImage: {
         'hero-pattern': "linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3)), url('/images/lisbon.jpg')",
+        'divider': "linear-gradient(90deg,transparent 0,#fff 50%,transparent)",
       },
       height: {
         '1/5-screen': '20vh',
